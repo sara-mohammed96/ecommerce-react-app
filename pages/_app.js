3f71df0bb870cf4055e50f1a02d3fe6a035ecec4
Sara Mohammed <sara.korea96@gmail.com>
2023-02-03 15:14:26 +0300

    working on the ecommerce project

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast'
+import { Layout } from '../components'
+import { StateContext } from '../context/StateContext'
+export default function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+     <Layout>
+      <Toaster />
+       <Component {...pageProps} />
+      </Layout> 
+    </StateContext>
+    
+  )
+ 
+}
